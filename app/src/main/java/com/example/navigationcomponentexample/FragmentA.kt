a8c15366de04ec5adfc5b7a57ddd907b65050ea5
chalius <gonzalo.quispe.f@tecsup.edu.pe>
2020-01-27 23:50:08 -0500

    primer commit

---
+++ app/src/main/java/com/example/navigationcomponentexample/FragmentA.kt
@@ -0,0 +1,39 @@
+package com.example.navigationcomponentexample
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_a.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FragmentA : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        btn_a.setOnClickListener {
+            findNavController().navigate(R.id.next_action)
+
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/navigationcomponentexample/FragmentB.kt
@@ -0,0 +1,40 @@
+package com.example.navigationcomponentexample
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_b.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FragmentB : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_b.setOnClickListener {
+
+
+
+            findNavController().navigate(R.id.next_action)
+
+        }
+
+    }
+
+
+}
